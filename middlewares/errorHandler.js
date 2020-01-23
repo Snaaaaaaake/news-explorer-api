b541aa3cc0abff58017ac556618247d2ade01cd5
Snaaaaaaake <kissmyshinyass@mail.ru>
2020-01-23 21:30:09 +0300

    добавил новое сообщение об ошибке

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,5 @@
-const { ErrorNotFound } = require('../modules/errors');
+const { ErrorNotFound, ErrorBadRequest } = require('../modules/errors');
+const { error400Message } = require('../configs/errorMessages');
 
 const notFound = (req, res, next) => {
   next(new ErrorNotFound());
@@ -6,10 +7,13 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   let incomingError = err;
-  // Обработка встроенной ошибки mongoose при запросе несуществующего документа,
-  // по другому к ней не подобраться
+  // Обработка встроенных ошибок mongoose,
+  // по другому к ним не подобраться
   if (/Cast to [a-z]+ failed/i.test(incomingError.message)) {
     incomingError = new ErrorNotFound();
+  }
+  if (/duplicate key error/i.test(incomingError.message)) {
+    incomingError = new ErrorBadRequest(error400Message.exist);
   }
   const { statusCode = 500, message } = incomingError;
   res.status(statusCode).send({ message, statusCode });
